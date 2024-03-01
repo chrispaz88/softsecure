08f0f02fe9bddcc827b917de5e7f5f496f2fb78b
Chris <christian.pazmino01@epn.edu.ec>
2024-03-01 04:02:21 -0500

    changes

--- softsecure/urls.py
+++ softsecure/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from ..salaries import views
 
 urlpatterns = [
+    path("", views.index, name="index"),
     path('admin/', admin.site.urls),
-    path('', include('salaries.urls')),
-
+    path('add_salary/', views.add_salary, name='add_salary'),
+    path('total_salaries/', views.total_decrypted_salaries, name='total_salaries'),
+    path('edit_salary/<str:salary_id>/', views.edit_salary, name='edit_salary'),
+    path('delete_salary/<str:salary_id>/', views.delete_salary, name='delete_salary'),
 ]
