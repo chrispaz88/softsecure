aedf5ab9538e2fda2ab556dd68dabab444f19653
Chris <christian.pazmino01@epn.edu.ec>
2024-03-01 04:37:48 -0500

    host

--- softsecure/settings.py
+++ softsecure/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your_secret_key')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'softsecure.herokuapp.com']
 
 RENDER_EXTERNAL_HOST = os.environ.get('RENDER_EXTERNAL_HOST')
 if RENDER_EXTERNAL_HOST:
